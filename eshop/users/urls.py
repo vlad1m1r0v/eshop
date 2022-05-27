ae1efebc0dc2b874f883a119351530b3900d1bea
= <mypochtav@gmail.com>
2022-05-27 12:05:33 +0300

    address -> addresses

--- eshop/users/urls.py
+++ eshop/users/urls.py
@@ -3,6 +3,6 @@ from .views import CurrentUserView, UserAddressesView, UserAddressView
 
 urlpatterns = [
     path('current-user', CurrentUserView.as_view(), name='users_current_user'),
-    path('current-user/address', UserAddressesView.as_view(), name='users_current_user_addresses'),
-    path('current-user/address/<user_address_id>', UserAddressView.as_view(), name='users_current_user_address')
+    path('current-user/addresses', UserAddressesView.as_view(), name='users_current_user_addresses'),
+    path('current-user/addresses/<user_address_id>', UserAddressView.as_view(), name='users_current_user_address')
 ]
