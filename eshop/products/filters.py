2aad6ee8efe8bb9ccc59703fc8fd38a312f01203
= <mypochtav@gmail.com>
2022-06-02 19:34:53 +0300

    product category filter works with category parent key

--- eshop/products/filters.py
+++ eshop/products/filters.py
@@ -1,15 +1,20 @@
 import django_filters
+from django.db.models import Q
+from django_filters import rest_framework as filters
 
 from .models import Product
-from django_filters import rest_framework as filters
 
 
 class ProductFilter(filters.FilterSet):
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
-    category = django_filters.CharFilter(field_name="category__name", lookup_expr='exact')
+    category = django_filters.CharFilter(method='category_filter', label="category")
     name = django_filters.CharFilter(field_name="name", lookup_expr='icontains')
 
     class Meta:
         model = Product
         fields = ['category', 'min_price', 'max_price']
+
+    def category_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(category__name__iexact=value) | Q(category__parent__name__iexact=value))
