1ed022826a7e79fa33277f79b7c5b910f4e72a21
= <mypochtav@gmail.com>
2022-06-26 15:49:36 +0300

    increment decrement amount of products is completed

--- eshop/carts/urls.py
+++ eshop/carts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import CartView, RetrieveDestroyWishListView
+from .views import CartView, RetrieveDestroyWishListView, IncrementView, DecrementView
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('<product_id>', RetrieveDestroyWishListView.as_view(), name='cart item'),
+    path('<product_id>', RetrieveDestroyWishListView.as_view(), name='cart_item'),
+    path('<product_id>/increment', IncrementView.as_view(), name='increment_item'),
+    path('<product_id>/decrement', DecrementView.as_view(), name='decrement_item'),
+
 ]
--- eshop/carts/views.py
+++ eshop/carts/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import RetrieveDestroyAPIView
+from rest_framework.generics import RetrieveDestroyAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from products.models import Product
 from .models import Cart, CartItem
 from .serializers import CartSerializer, CartItemSerializer
-from .permissions import IncrementPermission
+from .permissions import IncrementPermission, DecrementPermission
 
 
 # Create your views here.
@@ -44,3 +44,35 @@ class RetrieveDestroyWishListView(RetrieveDestroyAPIView):
     def get_object(self):
         cart_query = Cart.objects.get(user_profile__user=self.request.user)
         return get_object_or_404(CartItem, cart=cart_query, product_id=self.kwargs.get('product_id'))
+
+
+class IncrementView(APIView):
+    permission_classes = (IsAuthenticated, IncrementPermission,)
+
+    def patch(self, *args, **kwargs):
+        try:
+            cart_query = Cart.objects.get(user_profile__user=self.request.user)
+            cart_item_query = CartItem.objects.get(cart=cart_query, product_id=self.kwargs.get('product_id'))
+            cart_item_query.amount += 1
+            cart_item_query.save()
+            cart_item = CartItemSerializer(cart_item_query)
+            return Response(data=cart_item.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(data={'detail': 'Cart or product with given id not found.'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+
+class DecrementView(APIView):
+    permission_classes = (IsAuthenticated, DecrementPermission,)
+
+    def patch(self, *args, **kwargs):
+        try:
+            cart_query = Cart.objects.get(user_profile__user=self.request.user)
+            cart_item_query = CartItem.objects.get(cart=cart_query, product_id=self.kwargs.get('product_id'))
+            cart_item_query.amount -= 1
+            cart_item_query.save()
+            cart_item = CartItemSerializer(cart_item_query)
+            return Response(data=cart_item.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(data={'detail': 'Cart or product with given id not found.'},
+                            status=status.HTTP_404_NOT_FOUND)
